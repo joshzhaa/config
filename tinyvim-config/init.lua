6fcea2563095392c372d00693cb11024d24a237a
ssol <ssol@desktop>
2024-12-01 20:45:37 -0600

    record tinyvim config alongside lazyvim config

---
+++ tinyvim-config/init.lua
@@ -0,0 +1,31 @@
+require "options"
+require "mappings"
+require "commands"
+
+-- bootstrap plugins & lazy.nvim
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim" -- path where its going to be installed
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require "plugins"
+
+vim.cmd "colorscheme nightfox"
+
+function Transparent(color)
+  color = color or "nightfox"
+  vim.cmd.colorscheme(color)
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+Transparent()
