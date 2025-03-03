c97d28c3b16dead8fe1e242ff65206880cbc51dd
ssol <ssol@desktop>
2025-03-03 10:58:31 -0600

    lazyvim improvements - updated lazyvim and added windows clip

--- lazyvim-config/lua/config/options.lua
+++ lazyvim-config/lua/config/options.lua
@@ -3,3 +3,16 @@
 -- Add any additional options here
 
 vim.g.autoformat = false
+
+vim.g.clipboard = {
+  name = 'wsl-clip',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+}
+
