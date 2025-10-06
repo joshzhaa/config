f773a9e2c5a17a5063a5f6f7a49915b2fdae9363
josh <joshzhaa@gmail.com>
2025-10-06 04:20:18 +0000

    set default shell for neovim, lots of tight coupling here

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -9,6 +9,8 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
+vim.o.shell = "zsh"
+
 vim.g.clipboard = {
   name = 'wsl-clip',
   copy = {
