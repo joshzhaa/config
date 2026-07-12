65a841aae1182dca2b2a89119d9e6732dc9e94f7
josh <joshzhaa@gmail.com>
2026-07-11 17:28:59 -0700

    add some comments about the closure

--- nix/modules/overlays/kde.nix
+++ nix/modules/overlays/kde.nix
@@ -1,4 +1,11 @@
-# overlay trick adapted from mbleichner in https://github.com/NixOS/nixpkgs/issues/126590
+# Overlay trick adapted from mbleichner in https://github.com/NixOS/nixpkgs/issues/126590.
+# An interesting downstream effect of this approach is that your system will depend on two different
+# plasma-workspace packages, since the new pkgs-final.kdePackages.plasma-workspace depends on the
+# xdg-data package, which in turn depends on the pkgs-prev.kdePackages.plasma-workspace. Seeing this,
+# it may be possible to optimize this quite a bit by just pulling the old plasma-workspace from
+# the substituter while not actually overlaying another plasma-workspace into the pkgs set. We would
+# do this by creating a derivation that depends on plasma-workspace instead of overriding it. Then,
+# we would somehow replace the plasma-workspace package in the plasma nixos module.
 pkgs-final: pkgs-prev:
 let
   plasma-workspace-prev = pkgs-prev.kdePackages.plasma-workspace;
